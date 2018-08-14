3e65d6f055cc5b4ded6a7bb666170b0298f1946f
itsvictorj <janhivictor4@gmail.com>
2018-08-14 22:21:43 +0200

    Map functionality added showing UCT area.

--- app/src/main/java/com/example/jnhvic001/wifimapperv1/HomeScreen.java
+++ app/src/main/java/com/example/jnhvic001/wifimapperv1/HomeScreen.java
@@ -1,7 +1,10 @@
 package com.example.jnhvic001.wifimapperv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
-    }
-}
+
+        Button btn = findViewById(R.id.button1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this, MapsActivity.class));
+            }
+
+        });
+}}
